08516ff5ba7b8d70a22c1ff769ead95686af840c
AliyevMuhammedali <78734239+AliyevMuhammedali@users.noreply.github.com>
2025-05-26 19:49:28 +0400

    Update access.js

--- access.js
+++ access.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
     "JW9UB4I7E0AU", "92Q9ZW1YK92K", "7W8CJ23W3T0G", "J55OJCPZFWQ1", "WUSZKF3A9E1H"
   ];
 
-  const adminKeys = [ "BAKUSTALKER1", "KOLYUCHIY535";]
+  const adminKeys = ["BAKUSTALKER1", "KOLYUCHIY535"];
   const usedKeys = JSON.parse(localStorage.getItem("usedKeys")) || [];
 
   function requestAccess() {
